43b4e298797de8d75f15ee27019ce2ca905b169f
samaeen <sama.shoummo@gmail.com>
2019-07-11 23:21:20 +0600

    screenGrab done

---
+++ copyScreen.py
@@ -0,0 +1,45 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+import time
+from directkeys import PressKey, W, A, S, D
+
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def process_img(original_image):
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)
+    
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],
+                         ], np.int32)
+    processed_img = roi(processed_img, [vertices])
+    
+    return processed_img
+
+def main():
+    
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
+    last_time = time.time()
+    while True:
+        #PressKey(W)
+        screen =  np.array(ImageGrab.grab(bbox=(0,40,650,450)))
+        #print('Frame took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        new_screen = process_img(screen)
+        cv2.imshow('window', new_screen)
+        #cv2.imshow('window',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+main()
